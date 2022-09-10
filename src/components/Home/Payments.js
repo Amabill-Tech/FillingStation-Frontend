d37b22dc6b1dfcbecd3832fd231e3c169c34ffa2
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2022-09-10 07:29:39 -0700

    update

--- src/components/Home/Payments.js
+++ src/components/Home/Payments.js
@@ -1,9 +1,126 @@
 import React from 'react';
+import '../../styles/payments.scss';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 
 const Payments = () => {
     return(
-        <div style={{width:'100%', height:'100%', background:'green', display:'flex', justifyContent:"center", alignItems:'center'}}>Payments</div>
+        <div className='paymentsCaontainer'>
+            <div className='inner-pay'>
+                <div className='action'>
+                    <div style={{width:'150px'}} className='butt2'>
+                        <Select
+                            labelId="demo-select-small"
+                            id="demo-select-small"
+                            value={10}
+                            sx={{...selectStyle2, backgroundColor:"#F36A4C", color:'#fff'}}
+                        >
+                            <MenuItem value={10}>Add Payments</MenuItem>
+                            <MenuItem value={20}>Download PDF</MenuItem>
+                            <MenuItem value={30}>Print</MenuItem>
+                        </Select>
+                    </div>
+                </div>
+
+                <div className='search'>
+                    <div className='input-cont'>
+                        <div className='second-select'>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={10}
+                                sx={selectStyle2}
+                            >
+                                <MenuItem value={10}>07 August, 2022</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </div>
+                        <div className='second-select'>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={10}
+                                sx={selectStyle2}
+                            >
+                                <MenuItem value={10}>07 August, 2022</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                            </Select>
+                        </div>
+                    </div>
+                    <div className='butt'>
+                        <Button sx={{
+                            width:'100%', 
+                            height:'30px',  
+                            background: '#427BBE',
+                            borderRadius: '3px',
+                            fontSize:'12px',
+                            '&:hover': {
+                                backgroundColor: '#427BBE'
+                            }
+                            }}  variant="contained"> Add Payment
+                        </Button>
+                    </div>
+                </div>
+
+                <div className='search2'>
+                    <div className='butt2'>
+                        <Select
+                            labelId="demo-select-small"
+                            id="demo-select-small"
+                            value={10}
+                            sx={selectStyle2}
+                        >
+                            <MenuItem value={10}>Show entries</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </div>
+                    <div style={{width:'280px'}} className='input-cont2'>
+                        <div className='second-select2'>
+                            <Button sx={{
+                                width:'100%', 
+                                height:'30px',  
+                                background: '#58A0DF',
+                                borderRadius: '3px',
+                                fontSize:'12px',
+                                '&:hover': {
+                                    backgroundColor: '#58A0DF'
+                                }
+                                }}  variant="contained"> Download PDF
+                            </Button>
+                        </div>
+                        <div className='second-select2'>
+                            <Button sx={{
+                                width:'100%', 
+                                height:'30px',  
+                                background: '#F36A4C',
+                                borderRadius: '3px',
+                                fontSize:'12px',
+                                '&:hover': {
+                                    backgroundColor: '#F36A4C'
+                                }
+                                }}  variant="contained"> Print
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
+}
+
+const selectStyle2 = {
+    width:'100%', 
+    height:'35px', 
+    borderRadius:'5px',
+    background: '#F2F1F1B2',
+    color:'#000',
+    fontFamily: 'Nunito-Regular',
+    fontSize:'14px',
+    outline:'none'
 }
 
 export default Payments;
