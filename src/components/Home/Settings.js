d6fc75760302104542badf6dd988fd5db8f6b82e
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2022-09-13 02:16:39 -0700

    update

--- src/components/Home/Settings.js
+++ src/components/Home/Settings.js
@@ -8,6 +8,8 @@ import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Radio from '@mui/material/Radio';
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
 
 const Settings = ({history}) => {
 
@@ -270,7 +272,7 @@ const Settings = ({history}) => {
     const Password = () => {
         return(
             <div className='appearance'>
-                <div className='app'>
+                <div style={{width:'200px', marginTop:'10px'}} className='app'>
                     <div className='head'>Change Password</div>
                 </div>
                 <div className='details'>
@@ -299,15 +301,134 @@ const Settings = ({history}) => {
                             }} placeholder="" 
                         />
                     </div>
+
+                    <div style={{
+                        marginTop:'20px', 
+                        fontSize:'14px', 
+                        fontFamily:'Nunito-Regular'
+                        }} className='text-group'>Forgot password?
+                    </div>
+
+                    <div style={{marginTop:'20px'}} className='text-group'>
+                        <Button 
+                            variant="contained" 
+                            sx={{
+                                width:'100%',
+                                height:'30px',
+                                background:'blue',
+                                fontSize:'11px',
+                                marginTop:'20px',
+                                marginBottom:'20px',
+                                '&:hover': {
+                                    backgroundColor: 'blue'
+                                }
+                            }}
+                        >
+                            Save Changes
+                        </Button>
+                    </div>
                 </div>
             </div>
         )
     }
 
+    const IOSSwitch = styled((props) => (
+        <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+      ))(({ theme }) => ({
+        width: 42,
+        height: 26,
+        padding: 0,
+        '& .MuiSwitch-switchBase': {
+          padding: 0,
+          margin: 2,
+          transitionDuration: '300ms',
+          '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+              backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+              opacity: 1,
+              border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+              opacity: 0.5,
+            },
+          },
+          '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+          },
+          '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[600],
+          },
+          '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+          },
+        },
+        '& .MuiSwitch-thumb': {
+          boxSizing: 'border-box',
+          width: 22,
+          height: 22,
+        },
+        '& .MuiSwitch-track': {
+          borderRadius: 26 / 2,
+          backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+          opacity: 1,
+          transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+          }),
+        },
+    }));
+
     const Email = () => {
         return(
             <div className='appearance'>
+                <div style={{width:'200px', marginTop:'10px'}} className='app'>
+                    <div className='head'>Change Email</div>
+                </div>
+                <div className='details'>
+                    <div className='text-group'>
+                        <div className='form-text'>Email</div>
+                        <OutlinedInput 
+                            sx={{
+                                width:'100%',
+                                height: '35px', 
+                                marginTop:'5px', 
+                                background:'#EEF2F1', 
+                                border:'1px solid #777777'
+                            }} placeholder="" 
+                        />
+                    </div>
 
+                    <div style={emailSwith} className='text-group'>
+                        <IOSSwitch sx={{ m: 1 }} defaultChecked />
+                        <div style={email}>
+                            Recieve notification and messages through this email
+                        </div>
+                    </div>
+
+                    <div style={{marginTop:'20px'}} className='text-group'>
+                        <Button 
+                            variant="contained" 
+                            sx={{
+                                width:'100%',
+                                height:'30px',
+                                background:'blue',
+                                fontSize:'11px',
+                                marginTop:'20px',
+                                marginBottom:'20px',
+                                '&:hover': {
+                                    backgroundColor: 'blue'
+                                }
+                            }}
+                        >
+                            Save
+                        </Button>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -315,9 +436,60 @@ const Settings = ({history}) => {
     const DeleteOutlet = () => {
         return(
             <div className='appearance'>
+                <div style={{width:'200px', marginTop:'10px'}} className='app'>
+                    <div className='head'>Delete Outlet</div>
+                </div>
+                <div className='details'>
+                    <div className='text-group'>
+                        <div style={{fontFamily:'Nunito-Regular', fontWeight:'bold', fontSize:'14px'}}>Notice</div>
+                        <div style={{
+                            fontFamily:'Nunito-Regular', 
+                            fontSize:'12px', 
+                            marginTop:'10px',
+                            textAlign:'left'
+                        }}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere  
+                        </div>
+                    </div>
 
+                    <div style={{marginTop:'20px'}} className='text-group'>
+                        <div className='form-text'>Why do you want to delete outlets?</div>
+                        <OutlinedInput 
+                            sx={{
+                                width:'100%',
+                                height: '35px', 
+                                marginTop:'5px', 
+                                background:'#EEF2F1', 
+                                border:'1px solid #777777'
+                            }} placeholder="" 
+                        />
+                    </div>
+
+                    <div style={{marginTop:'20px'}} className='text-group'>
+                        <Button 
+                            variant="contained" 
+                            sx={{
+                                width:'100%',
+                                height:'30px',
+                                background:'blue',
+                                fontSize:'11px',
+                                marginTop:'20px',
+                                marginBottom:'20px',
+                                '&:hover': {
+                                    backgroundColor: 'blue'
+                                }
+                            }}
+                        >
+                            Save
+                        </Button>
+                    </div>
+                </div>
             </div>
         )
+    }
+
+    const logout = () => {
+        
     }
 
     return(
@@ -351,30 +523,30 @@ const Settings = ({history}) => {
                 <div className='leftSettings'>
                     <div className='linspace'>
                         <div onClick={()=>{setNav(0)}} className='accord'>
-                            <div className='text'>Outlet Information</div>
+                            <div style={nav === 0? active: inActive}>Outlet Information</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
                         <div onClick={()=>{setNav(1)}} className='accord'>
-                            <div style={{color:'#1F1F1F'}} className='text'>Appearances</div>
+                            <div style={nav === 1? active: inActive} className='text'>Appearances</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
                         <div onClick={()=>{setNav(2)}} className='accord'>
-                            <div style={{color:'#1F1F1F'}} className='text'>Logo ( Branding )</div>
+                            <div style={nav === 2? active: inActive} className='text'>Logo ( Branding )</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
                         <div onClick={()=>{setNav(3)}} className='accord'>
-                            <div style={{color:'#1F1F1F'}} className='text'>Change Password</div>
+                            <div style={nav === 3? active: inActive} className='text'>Change Password</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
                         <div onClick={()=>{setNav(4)}} className='accord'>
-                            <div style={{color:'#1F1F1F'}} className='text'>Change Email</div>
+                            <div style={nav === 4? active: inActive} className='text'>Change Email</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
                         <div onClick={()=>{setNav(5)}} className='accord'>
-                            <div style={{color:'#1F1F1F'}} className='text'>Delete Outlets</div>
+                            <div style={nav === 5? active: inActive} className='text'>Delete Outlets</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
-                        <div onClick={()=>{history.push('/login')}} className='accord'>
+                        <div onClick={logout} className='accord'>
                             <div style={{color:'#1F1F1F'}} className='text'>Logout</div>
                             <img style={{width:'7px', height:'13px'}} src={rightArrow} alt="icon" />
                         </div>
@@ -393,6 +565,37 @@ const Settings = ({history}) => {
             </div>
         </div>
     )
+}
+
+const emailSwith = {
+    marginTop:'30px',
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center'
+}
+
+const email = {
+    fontSize:'12px',
+    fontFamily:'Nunito-Regular',
+    marginLeft:'10px'
+}
+
+const active = {
+    fontFamily: 'Nunito-Regular',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: '12px',
+    lineHeight: '150.4%',
+    color: '#06805B',
+}
+
+const inActive = {
+    fontFamily: 'Nunito-Regular',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: '12px',
+    lineHeight: '150.4%',
+    color: '#1F1F1F',
 }
 
 export default Settings;
