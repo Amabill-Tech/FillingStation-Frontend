cb83d814c4373507f3145e304297636e698f1811
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2022-09-15 00:34:17 -0700

    update

--- src/components/RecordSales/Expenses.js
+++ src/components/RecordSales/Expenses.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import '../../styles/expenses.scss';
 
 const Expenses = () => {
     return(
-        <div>Expenses</div>
+        <div className='expensesContainer'>
+            <div className='inputs'>
+                <div className='text'>Date Created</div>
+                <input className='date' type={'date'}  />
+            </div>
+
+            <div className='twoInputs'>
+                <div className='inputs2'>
+                    <div className='text'>Expenses Name</div>
+                    <input className='date' type={'text'}  />
+                </div>
+
+                <div className='inputs2'>
+                    <div className='text'>Expenses Date</div>
+                    <input className='date' type={'date'}  />
+                </div>
+            </div>
+
+            <div style={{marginTop:'20px'}} className='inputs'>
+                <div className='text'>Description</div>
+                <textarea style={textAreaStyle} className='date' type={'date'}  />
+            </div>
+
+            <div style={{marginTop:'20px'}} className='inputs'>
+                <div className='text'>Expenses Amount</div>
+                <input className='date' type={'text'}  />
+            </div>
+        </div>
     )
+}
+
+const textAreaStyle = {
+    height:'150px',
+    paddingTop:'10px',
 }
 
 export default Expenses;
