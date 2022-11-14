5a085a202d250e429dbe3d7b1daf3b89e5901b26
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2022-11-14 03:53:46 -0800

    update

--- src/components/DailySales/ComprehensiveReport.js
+++ src/components/DailySales/ComprehensiveReport.js
@@ -175,7 +175,7 @@ const PMSDailySales = (props) => {
 const LPODailySales = () => {
     return(
         <div>
-            <div>LPO</div>
+            <div style={{width:'100%', textAlign:'left', marginBottom:'10px', color:'#06805B', fontSize:'12px', fontWeight:'900'}}>LPO</div>
             <div className='main-sales'>
                 <div className='inner'>
                     <div className='table-heads'>
@@ -223,6 +223,127 @@ const LPODailySales = () => {
                         <div style={{background: "transparent"}} className='col'></div>
                         <div style={{background: "transparent"}} className='col'></div>
                         <div style={{background: "transparent"}} className='col'></div>
+                        <div style={{background: "transparent"}} className='col'></div>
+                        <div className='col'>Total</div>
+                        <div style={{marginRight:'0px'}} className='col'>435, 000</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ExpensesDailySales = () => {
+    return(
+        <div>
+            <div style={{width:'100%', textAlign:'left', marginBottom:'10px', color:'#06805B', fontSize:'12px', fontWeight:'900'}}>Expenses</div>
+            <div style={{width:'350px'}} className='main-sales'>
+                <div className='inner'>
+                    <div className='table-heads'>
+                        <div className='col'>S/N</div>
+                        <div className='col'>Expense Name</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div className='col'>1</div>
+                        <div className='col'>Rate</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div className='col'>2</div>
+                        <div className='col'>Rate</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div className='col'>3</div>
+                        <div className='col'>Rate</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div style={{background: "transparent"}} className='col'></div>
+                        <div className='col'>Total</div>
+                        <div style={{marginRight:'0px'}} className='col'>435, 000</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ExpensesSummary = () => {
+    return(
+        <div>
+            <div style={{width:'100%', textAlign:'left', marginBottom:'10px', color:'#06805B', fontSize:'12px', fontWeight:'900'}}></div>
+            <div style={{width:'350px'}} className='main-sales'>
+                <div className='inner'>
+                    <div className='table-heads'>
+                        <div style={{width:'70%', display:'flex', justifyContent:'flex-start'}} className='col'>
+                            <span style={{marginLeft:'10px'}}>Total Amount of sales (NGN)</span>
+                        </div>
+                        <div style={{marginRight:'0px', width:'30%', background:'#EDEDED', color:'#000'}} className='col'>Amount</div>
+                    </div>
+
+                    <div style={{marginTop:'5px'}} className='table-heads'>
+                        <div style={{width:'70%', display:'flex', justifyContent:'flex-start'}} className='col'>
+                            <span style={{marginLeft:'10px'}}>Total Amount of Expenses (NGN)</span>
+                        </div>
+                        <div style={{marginRight:'0px', width:'30%', background:'#EDEDED', color:'#000'}} className='col'>Amount</div>
+                    </div>
+
+                    <div style={{marginTop:'5px'}} className='table-heads'>
+                        <div style={{width:'70%', display:'flex', justifyContent:'flex-start'}} className='col'>
+                            <span style={{marginLeft:'10px'}}>Total Amount of I.O.N (NGN)</span>
+                        </div>
+                        <div style={{marginRight:'0px', width:'30%', background:'#EDEDED', color:'#000'}} className='col'>Amount</div>
+                    </div>
+
+                    <div style={{marginTop:'5px'}} className='table-heads2'>
+                        <div style={{width:'70%', display:'flex', justifyContent:'flex-end'}} className='col'>
+                            <span style={{marginRight:'20px'}}>Total</span>
+                        </div>
+                        <div style={{marginRight:'0px', width:'30%'}} className='col'>Amount</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const PaymentDailySales = () => {
+    return(
+        <div>
+            <div style={{width:'100%', textAlign:'left', marginBottom:'10px', color:'#06805B', fontSize:'12px', fontWeight:'900'}}>Payments</div>
+            <div style={{width:'350px'}} className='main-sales'>
+                <div className='inner'>
+                    <div className='table-heads'>
+                        <div className='col'>S/N</div>
+                        <div className='col'>Expense Name</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div className='col'>1</div>
+                        <div className='col'>Rate</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div className='col'>2</div>
+                        <div className='col'>Rate</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
+                        <div className='col'>3</div>
+                        <div className='col'>Rate</div>
+                        <div style={{marginRight:'0px'}} className='col'>Amount</div>
+                    </div>
+
+                    <div className='table-heads2'>
                         <div style={{background: "transparent"}} className='col'></div>
                         <div className='col'>Total</div>
                         <div style={{marginRight:'0px'}} className='col'>435, 000</div>
@@ -288,6 +409,16 @@ const ComprehensiveReport = () => {
                         <PMSDailySales name={'AGO'} />
                         <PMSDailySales name={'DPK'} />
                         <LPODailySales />
+                        <ExpensesDailySales />
+
+                        <div className='paym'>
+                            <div className='pleft'>
+                                <ExpensesSummary />
+                            </div>
+                            <div className='pleft'>
+                                <PaymentDailySales />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='right'>world</div>
